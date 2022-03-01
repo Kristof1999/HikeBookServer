68f9d95e3a4183a11508c59dd9d7c58ad6eef4b5
Nagy Kristof <nagykristof958@gmail.com>
2022-03-01 07:17:03 +0100

    Initial commit

---
+++ src/main/java/hu/kristof/nagy/hikebookserver/HikeBookServerApplication.java
@@ -0,0 +1,13 @@
+package hu.kristof.nagy.hikebookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HikeBookServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HikeBookServerApplication.class, args);
+	}
+
+}
