bad7d541c5602e30db142b72dcdcdddd097ef248
Nagy Kristof <nagykristof958@gmail.com>
2022-03-21 11:47:20 +0100

    Upgraded auth tests to clean the database before tests

--- src/test/java/hu/kristof/nagy/hikebookserver/service/LoginTest.java
+++ src/test/java/hu/kristof/nagy/hikebookserver/service/LoginTest.java
@@ -1,12 +1,21 @@
 package hu.kristof.nagy.hikebookserver.service;
 
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import hu.kristof.nagy.hikebookserver.data.DbPathConstants;
 import hu.kristof.nagy.hikebookserver.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.ExecutionException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+// For local emulator suite: set environment variables:
+// FIRESTORE_EMULATOR_HOST=localhost:8000
+// GCLOUD_PROJECT=hikebook
 
 @SpringBootTest
 class LoginTest {
@@ -17,24 +26,41 @@ class LoginTest {
     @Autowired
     private RegisterService register;
 
+    @Autowired
+    private Firestore db;
+
     @BeforeEach
     void setUp() {
-
+        for(DocumentReference doc : db.collection(DbPathConstants.COLLECTION_USER).listDocuments()) {
+            try {
+                doc.delete().get();
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Test
     void testCorrectLogin() {
+        register.registerUser(new User("asd", "asd"));
+
         boolean res = login.loginUser(new User("asd","asd"));
+
         assertTrue(res);
     }
 
     @Test
     void testIncorrectLogin() {
         boolean res = login.loginUser(new User("a", "asd"));
+
         assertFalse(res);
+
         res = login.loginUser(new User("a", "a"));
+
         assertFalse(res);
+
         res = login.loginUser(new User("asd", "a"));
+
         assertFalse(res);
     }
 }
--- src/test/java/hu/kristof/nagy/hikebookserver/service/RegisterTest.java
+++ src/test/java/hu/kristof/nagy/hikebookserver/service/RegisterTest.java
@@ -1,10 +1,17 @@
 package hu.kristof.nagy.hikebookserver.service;
 
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import hu.kristof.nagy.hikebookserver.data.DbPathConstants;
 import hu.kristof.nagy.hikebookserver.model.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -13,15 +20,34 @@ class RegisterTest {
     @Autowired
     private RegisterService register;
 
+    @Autowired
+    private Firestore db;
+
+    @BeforeEach
+    void setUp() {
+        for(DocumentReference doc : db.collection(DbPathConstants.COLLECTION_USER).listDocuments()) {
+            try {
+                doc.delete().get();
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
     @Test
-    private void testCorrectRegistration() {
+    void testCorrectRegistration() {
         boolean res = register.registerUser(new User("asd", "asd"));
+
         assertTrue(res);
     }
 
     @Test
-    private void testIncorrectRegistration() {
-        boolean res = register.registerUser(new User("asd", "asd"));
-        assertTrue(res);
+    void testIncorrectRegistration() {
+        User user = new User("asd", "asd");
+        register.registerUser(user);
+
+        boolean res = register.registerUser(user);
+
+        assertFalse(res);
     }
 }
