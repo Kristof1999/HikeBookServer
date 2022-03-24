b13cb3f7f305e2fb28c1c8676d720dbb2f55aeb8
Nagy Kristof <nagykristof958@gmail.com>
2022-03-24 11:05:18 +0100

    Added new test cases

--- src/test/java/hu/kristof/nagy/hikebookserver/service/UserRouteEditTest.java
+++ src/test/java/hu/kristof/nagy/hikebookserver/service/UserRouteEditTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -33,7 +34,7 @@ public class UserRouteEditTest {
     }
 
     @Test
-    void testNoChange() {
+    void testSameRouteNameSamePoints() {
         List<Point> points = new ArrayList<>();
         points.add(new Point(0.0, 0.0, PointType.SET, ""));
         points.add(new Point(1.0, 1.0, PointType.NEW, ""));
@@ -42,9 +43,26 @@ public class UserRouteEditTest {
         routeCreateService.createUserRoute(new UserRoute(userName, routeName, points, ""));
 
         UserRoute editedUserRoute = new UserRoute(userName, routeName, points, "");
+
+        assertThrows(IllegalArgumentException.class,
+                () -> routeEditService.editUserRoute(routeName, editedUserRoute)
+        );
+    }
+
+    @Test
+    void testSameRouteNameDifferentPoints() {
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(0.0, 0.0, PointType.SET, ""));
+        points.add(new Point(1.0, 1.0, PointType.NEW, ""));
+        String userName = "asd";
+        String routeName = "route";
+        routeCreateService.createUserRoute(new UserRoute(userName, routeName, points, ""));
+
+        points.add(new Point(2.0, 2.0, PointType.NEW, ""));
+        UserRoute editedUserRoute = new UserRoute(userName, routeName, points, "");
         boolean res = routeEditService.editUserRoute(routeName, editedUserRoute);
 
-        assertFalse(res);
+        assertTrue(res);
     }
 
     @Test
@@ -75,7 +93,7 @@ public class UserRouteEditTest {
                 new UserRoute(userName, routeName, points, "")
         );
 
-        points.remove(0);
+        points.add(new Point(2.0, 2.0, PointType.NEW, ""));
         UserRoute editedUserRoute = new UserRoute(userName, routeName + "2", points, "");
         boolean res = routeEditService.editUserRoute(routeName, editedUserRoute);
 
@@ -88,5 +106,60 @@ public class UserRouteEditTest {
                 () -> routeEditService.editUserRoute("",
                 new UserRoute("", "", new ArrayList<>(), "")
         ));
+    }
+
+    @Test
+    void testPointsExist() {
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(0.0, 0.0, PointType.SET, ""));
+        points.add(new Point(1.0, 1.0, PointType.NEW, ""));
+        String userName = "asd";
+        String routeName = "route";
+        routeCreateService.createUserRoute(
+                new UserRoute(userName, routeName, points, "")
+        );
+        String routeName2 = routeName + "2";
+        List<Point> points2 = new ArrayList<>();
+        for (Point p : points) {
+            points2.add(p);
+        }
+        points2.add(new Point(2.0, 2.0, PointType.NEW, ""));
+        routeCreateService.createUserRoute(
+                new UserRoute(userName, routeName2, points2, "")
+        );
+
+        points.add(new Point(2.0, 2.0, PointType.NEW, ""));
+        UserRoute editedUserRoute = new UserRoute(userName, routeName, points, "");
+
+        assertThrows(IllegalArgumentException.class,
+                () -> routeEditService.editUserRoute(routeName, editedUserRoute)
+        );
+    }
+
+    @Test
+    void testRouteNameExist() {
+        List<Point> points = new ArrayList<>();
+        points.add(new Point(0.0, 0.0, PointType.SET, ""));
+        points.add(new Point(1.0, 1.0, PointType.NEW, ""));
+        String userName = "asd";
+        String routeName = "route";
+        routeCreateService.createUserRoute(
+                new UserRoute(userName, routeName, points, "")
+        );
+        String routeName2 = routeName + "2";
+        List<Point> points2 = new ArrayList<>();
+        for (Point p : points) {
+            points2.add(p);
+        }
+        points2.add(new Point(2.0, 2.0, PointType.NEW, ""));
+        routeCreateService.createUserRoute(
+                new UserRoute(userName, routeName2, points2, "")
+        );
+
+        UserRoute editedUserRoute = new UserRoute(userName, routeName2, points, "");
+
+        assertThrows(IllegalArgumentException.class,
+                () -> routeEditService.editUserRoute(routeName, editedUserRoute)
+        );
     }
 }
