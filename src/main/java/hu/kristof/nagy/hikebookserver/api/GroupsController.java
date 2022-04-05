7059d80fd4f5fc706ca914a5de7220e0fa7e82a5
Nagy Kristof <nagykristof958@gmail.com>
2022-04-05 15:28:28 +0200

    Improved group creation

--- src/main/java/hu/kristof/nagy/hikebookserver/api/GroupsController.java
+++ src/main/java/hu/kristof/nagy/hikebookserver/api/GroupsController.java
@@ -3,10 +3,7 @@ package hu.kristof.nagy.hikebookserver.api;
 import hu.kristof.nagy.hikebookserver.service.groups.GroupCreateService;
 import hu.kristof.nagy.hikebookserver.service.groups.GroupsListService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,8 +26,9 @@ public class GroupsController {
 
     @PutMapping("groups/{groupName}")
     public boolean createGroup(
-            @PathVariable String groupName
+            @PathVariable String groupName,
+            @RequestBody String userName
     ) {
-        return groupCreateService.createGroup(groupName);
+        return groupCreateService.createGroup(groupName, userName);
     }
 }
--- src/main/java/hu/kristof/nagy/hikebookserver/service/groups/GroupCreateService.java
+++ src/main/java/hu/kristof/nagy/hikebookserver/service/groups/GroupCreateService.java
@@ -1,7 +1,8 @@
 package hu.kristof.nagy.hikebookserver.service.groups;
 
-import com.google.api.client.util.ArrayMap;
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QuerySnapshot;
 import hu.kristof.nagy.hikebookserver.data.DbPathConstants;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -19,22 +21,58 @@ public class GroupCreateService {
     @Autowired
     private Firestore db;
 
-    public boolean createGroup(String name) {
-        if (isNameUnique(name)) {
-            Map<String, Object> data = new HashMap<>();
-            data.put(DbPathConstants.GROUP_NAME, name);
+    public boolean createGroup(String groupName, String userName) {
+        if (isNameUnique(groupName)) {
             try {
-                db.collection(DbPathConstants.COLLECTION_GROUP)
-                        .document(name)
-                        .set(data)
-                        .get();
+                saveGroup(groupName);
+                saveMemberData(groupName, userName);
+                updateUserGroups(groupName, userName);
             } catch (InterruptedException | ExecutionException e) {
                 e.printStackTrace();
             }
             return true;
         } else {
-            throw new IllegalArgumentException("A " + name + " már létezik! Kérem, hogy válasszon másikat.");
+            throw new IllegalArgumentException("A " + groupName + " már létezik! Kérem, hogy válasszon másikat.");
         }
+    }
+
+    private void saveGroup(String groupName) throws ExecutionException, InterruptedException {
+        Map<String, Object> data = new HashMap<>();
+        data.put(DbPathConstants.GROUP_NAME, groupName);
+        db.collection(DbPathConstants.COLLECTION_GROUP)
+                .document(groupName)
+                .set(data)
+                .get();
+    }
+
+    private void saveMemberData(String groupName, String userName) throws ExecutionException, InterruptedException {
+        Map<String, Object> memberData = new HashMap<>();
+        memberData.put(DbPathConstants.GROUP_DETAILS_MEMBER_NAMES, List.of(userName));
+        db.collection(DbPathConstants.COLLECTION_GROUP)
+                .document(groupName)
+                .collection(DbPathConstants.GROUP_DETAILS)
+                .document(DbPathConstants.GROUP_DETAILS)
+                .set(memberData)
+                .get();
+    }
+
+    private void updateUserGroups(String groupName, String userName) throws ExecutionException, InterruptedException {
+        DocumentReference groupNamesDoc = db
+                .collection(DbPathConstants.COLLECTION_USER)
+                .document(userName)
+                .collection(DbPathConstants.USER_DETAILS)
+                .document(DbPathConstants.USER_DETAILS);
+        ApiFuture<DocumentSnapshot> future = groupNamesDoc.get();
+        List<String> groupNames = (List<String>) future.get().get(DbPathConstants.USER_DETAILS_GROUP_NAMES);
+        Map<String, Object> groupNamesData = new HashMap<>();
+        if (groupNames == null) {
+            groupNames = List.of(groupName);
+        } else {
+            groupNames.add(groupName);
+        }
+        groupNamesData.put(DbPathConstants.USER_DETAILS_GROUP_NAMES, groupNames);
+        groupNamesDoc.set(groupNamesData)
+                .get();
     }
 
     private boolean isNameUnique(String name) {
