31cfbec0a2a557dc79e6ded9c2ca124f3264a131
Nagy Kristof <nagykristof958@gmail.com>
2022-04-22 14:49:41 +0200

    Refactoring

--- src/test/java/hu/kristof/nagy/hikebookserver/service/TestUtils.java
+++ src/test/java/hu/kristof/nagy/hikebookserver/service/TestUtils.java
@@ -7,18 +7,20 @@ import hu.kristof.nagy.hikebookserver.data.DbPathConstants;
 import java.util.concurrent.ExecutionException;
 
 public class TestUtils {
+    public static void cleanGroups(Firestore db) {
+        cleanCollection(db, DbPathConstants.COLLECTION_GROUP);
+    }
+
     public static void cleanUsers(Firestore db) {
-        for(DocumentReference doc : db.collection(DbPathConstants.COLLECTION_USER).listDocuments()) {
-            try {
-                doc.delete().get();
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        cleanCollection(db, DbPathConstants.COLLECTION_USER);
     }
 
     public static void cleanRoutes(Firestore db) {
-        for(DocumentReference doc : db.collection(DbPathConstants.COLLECTION_ROUTE).listDocuments()) {
+        cleanCollection(db, DbPathConstants.COLLECTION_ROUTE);
+    }
+
+    private static void cleanCollection(Firestore db, String collection) {
+        for(DocumentReference doc : db.collection(collection).listDocuments()) {
             try {
                 doc.delete().get();
             } catch (ExecutionException | InterruptedException e) {
