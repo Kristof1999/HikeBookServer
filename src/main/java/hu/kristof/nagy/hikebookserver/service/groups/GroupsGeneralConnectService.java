f84d768db8e82950ba49614a5e2f5e546439b5bd
Nagy Kristof <nagykristof958@gmail.com>
2022-06-21 14:20:32 +0200

    Deleting group routes when there are no members left.

--- src/main/java/hu/kristof/nagy/hikebookserver/service/groups/GroupsGeneralConnectService.java
+++ src/main/java/hu/kristof/nagy/hikebookserver/service/groups/GroupsGeneralConnectService.java
@@ -1,12 +1,14 @@
 package hu.kristof.nagy.hikebookserver.service.groups;
 
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.Transaction;
 import hu.kristof.nagy.hikebookserver.data.DbPathConstants;
 import hu.kristof.nagy.hikebookserver.model.Group;
 import hu.kristof.nagy.hikebookserver.model.ResponseResult;
 import hu.kristof.nagy.hikebookserver.service.FutureUtil;
 import hu.kristof.nagy.hikebookserver.service.Util;
 import hu.kristof.nagy.hikebookserver.service.route.QueryException;
+import hu.kristof.nagy.hikebookserver.service.route.grouproute.GroupRouteDeleteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -64,23 +66,56 @@ public class GroupsGeneralConnectService {
     }
 
     private boolean disconnect(String groupName, String userName) {
-        var queryFuture = db
-                .collection(DbPathConstants.COLLECTION_GROUP)
-                .whereEqualTo(DbPathConstants.GROUP_NAME, groupName)
-                .whereEqualTo(DbPathConstants.GROUP_MEMBER_NAME, userName)
-                .get();
-        var queryDocs = FutureUtil.handleFutureGet(() ->
-                queryFuture.get().getDocuments()
-        );
-        return Util.handleListSize(queryDocs, documentSnapshots -> {
-            String id = documentSnapshots.get(0).getId();
-            FutureUtil.handleFutureGet(() -> db
+        var futureResult = db.runTransaction(transaction -> {
+            var query = db
                     .collection(DbPathConstants.COLLECTION_GROUP)
-                    .document(id)
-                    .delete()
-                    .get()
+                    .whereEqualTo(DbPathConstants.GROUP_NAME, groupName)
+                    .whereEqualTo(DbPathConstants.GROUP_MEMBER_NAME, userName);
+            var queryFuture = transaction.get(query);
+            var queryDocs = FutureUtil.handleFutureGet(() ->
+                    queryFuture.get().getDocuments()
             );
-            return true;
+
+            return Util.handleListSize(queryDocs, documentSnapshots -> {
+                String id = documentSnapshots.get(0).getId();
+                var docRef = db
+                        .collection(DbPathConstants.COLLECTION_GROUP)
+                        .document(id);
+
+                if (getNumberOfMembers(groupName, transaction) == 1) {
+                    deleteGroupRoutes(groupName, transaction);
+                }
+
+                transaction.delete(docRef);
+                return true;
+            });
         });
+
+        return FutureUtil.handleFutureGet(futureResult::get);
+    }
+
+    private int getNumberOfMembers(String groupName, Transaction transaction) {
+        var query = db
+                .collection(DbPathConstants.COLLECTION_GROUP)
+                .whereEqualTo(DbPathConstants.GROUP_NAME, groupName);
+        var queryFuture = transaction.get(query);
+        return FutureUtil.handleFutureGet(() -> queryFuture
+                .get()
+                .getDocuments()
+        ).size();
+    }
+
+    private void deleteGroupRoutes(String groupName, Transaction transaction) {
+        var query = db
+                .collection(DbPathConstants.COLLECTION_ROUTE)
+                .whereEqualTo(DbPathConstants.ROUTE_GROUP_NAME, groupName);
+        var queryFuture = transaction.get(query);
+        var groupRoutes = FutureUtil.handleFutureGet(() -> queryFuture
+                .get()
+                .getDocuments()
+        );
+        groupRoutes.forEach(queryDocumentSnapshot ->
+                transaction.delete(queryDocumentSnapshot.getReference())
+        );
     }
 }
